d820ee0eff354088b14d71c5ca5f68bc7285ee46
cicada-3301-vzyr <110697383+cicada-3301-vzyr@users.noreply.github.com>
2022-08-06 03:11:19 +0530

    Update Collision.js

--- Engine/Core/Collision.js
+++ Engine/Core/Collision.js
@@ -345,8 +345,18 @@ function resolveCollision(shape1, shape2, collisionInfo, positionalCorrectionPer
     // debugger;
     shape1.velocity.add(tangent.copy().scale(-shape1.invMass));
     shape2.velocity.add(tangent.copy().scale(shape2.invMass));
-    if(shape1.velocity.magSq()<=0.005){ shape1.velocity.scale(0);}
-    if(shape2.velocity.magSq()<=0.005){ shape2.velocity.scale(0);}
+    
+
+
+
+
+
+
+
+    if(shape1.velocity.magSq()<=0.01){ shape1.velocity.scale(0);}
+    if(shape2.velocity.magSq()<=0.01){ shape2.velocity.scale(0);}
+    if(shape1.angularVelocity<=0.006){ shape1.angularVelocity =0;}
+    if(shape2.angularVelocity<=0.006){ shape2.angularVelocity =0;}
 
 
     // //TODO : remove this
